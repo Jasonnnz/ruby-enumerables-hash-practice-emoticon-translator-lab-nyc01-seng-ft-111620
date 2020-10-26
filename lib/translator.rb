3759218d3d6946802125f375e13152680cd43f09
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-26 01:51:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_english_meaning(file, emoticon)
   # code goes here
   emojis = load_library(file)
   emojis.each do |emotion, values|
-    if emojis[emotion][:japanese].include? emoticon == false 
+    if !emojis.has_value?(emoticon)
       puts "Sorry, that emoticon was not found"
     end
   end  
